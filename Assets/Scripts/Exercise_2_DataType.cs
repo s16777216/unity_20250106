4bed5800616e072b114499c5f55e478283ecc897
s16777216 <l1597532@gmail.com>
2025-01-13 20:01:47 +0800

    新增作業2-資料型別

---
+++ Assets/Scripts/Exercise_2_DataType.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class Exercise_2_DataType : MonoBehaviour
+{
+    public float var1 = 7.7f;
+    public double var2 = 0.12345;
+    public decimal var3 = 1.23456789m;
+}
