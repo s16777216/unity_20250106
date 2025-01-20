8abb16dffd2210fef42a5f120c09ba6ebb87a87c
s16777216 <l1597532@gmail.com>
2025-01-20 20:04:04 +0800

    新增 課程4方法

---
+++ Assets/Scripts/Exercise_4_Function.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Exercise_4_Function : MonoBehaviour
+{
+    bool Func1()
+    {
+        Debug.Log("I am Func1.");
+        return true;
+    }
+
+    bool Func2() {
+        Debug.Log("I am Func2.");
+        return false;
+    }
+}
