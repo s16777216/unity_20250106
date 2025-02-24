e5bda91509d4bb681d0e1e04b1a058cc05ceb625
s16777216 <l1597532@gmail.com>
2025-02-24 19:57:08 +0800

    新增 練習9

---
+++ Assets/Scripts/Exercise_9.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Exercise_9 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        double var1 = -999.321;
+        Debug.Log(var1);
+        sbyte var2 = ((sbyte)var1);
+        Debug.Log(var2);
+
+    }
+
+}
